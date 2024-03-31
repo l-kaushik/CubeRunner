083dcfc3ef38a09e9353e2ae4fa0f39db0c8d81b
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-03-31 15:58:22 +0530

    converted OnComponentBeginOverlap
    1. Create class for BP_DeathWall
    2. Implement OnComponentBeginOverlap event in C++
    3. Minor fixes, header includes and file creations.

---
+++ Source/CubeRunner/DeathWall.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "DeathWall.h"
+
+// Sets default values
+ADeathWall::ADeathWall()
+{
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ADeathWall::OnBoxBeginOverlap);
+
+	WallMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CubeMesh"));
+	WallMesh->SetupAttachment(BoxCollision);
+}
+
+// Called when the game starts or when spawned
+void ADeathWall::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADeathWall::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (Cast<ACharacter>(OtherActor) == (UGameplayStatics::GetPlayerCharacter(GetWorld(), 0))) {
+		OtherActor->AddActorWorldOffset(FVector(0.0f, 0.0f, 0.0f));
+
+		UE_LOG(LogTemp, Warning,TEXT("Cube is being overlapped"))
+	}
+}
+
---
+++ Source/CubeRunner/GameCore.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameCore.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBERUNNER_API AGameCore : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
