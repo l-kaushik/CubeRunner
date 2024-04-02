b75bd349ff3184c0197866a2a3067434bc52e697
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-04-03 02:01:05 +0530

    create interface class
    Declare SetScore function

---
+++ Source/CubeRunner/GameStateInterface.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GameStateInterface.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable)
+class UGameStateInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+// Actual Interface Class
+class IGameStateInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void SetScore(int Score);
+	
+};
