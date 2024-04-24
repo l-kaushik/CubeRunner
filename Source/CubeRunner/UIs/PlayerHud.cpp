dd3ce8f28dd14b531657101d0e772b8ff0d65334
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-04-24 17:55:58 +0530

    Implement functions
    Create UpdateScore and UpdateTimer functions for hud widget.

--- Source/CubeRunner/UIs/PlayerHud.cpp
+++ Source/CubeRunner/UIs/PlayerHud.cpp
@@ -1,5 +1,35 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "kismet/KismetStringLibrary.h"
+#include "kismet/KismetTextLibrary.h"
+#include "Components/EditableText.h"
 #include "PlayerHud.h"
 
+void UPlayerHud::UpdateScore(int32 Score)
+{
+	if (PlayerScore)
+	{
+		//FString FirstPart = "Score: ";
+		//PlayerScore->SetText(UKismetTextLibrary::Conv_StringToText((FirstPart.Append(UKismetStringLibrary::Conv_IntToString(Score)))));
+
+		PlayerScore->SetText(FText::Format(INVTEXT("Score: {0}"), Score));
+	}
+}
+	
+
+void UPlayerHud::UpdateTime(int32 Time)
+{
+	if (Time == -1)
+	{
+		//return UKismetTextLibrary::Conv_StringToText(FString("Wall Spawned!"));
+		LastScoreHitTimer->SetText(FText::Format(INVTEXT("Time: {0}"), Time));
+	}
+	else
+	{
+	/*	FString FirstPart = "Time: ";
+		LastScoreHitTimer->SetText(UKismetTextLibrary::Conv_StringToText((FirstPart.Append(UKismetStringLibrary::Conv_IntToString(Time)))));*/
+
+		LastScoreHitTimer->SetText(FText::Format(INVTEXT("Time: {0}"), Time));
+	}
+}
--- Source/CubeRunner/UIs/PlayerHud.h
+++ Source/CubeRunner/UIs/PlayerHud.h
@@ -13,5 +13,19 @@ UCLASS()
 class CUBERUNNER_API UPlayerHud : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
 	
+	// update score on hud
+	void UpdateScore(int32 Score);
+
+	// update timer on hud
+	void UpdateTime(int32 Time);
+	
+	// widget to display score
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	class UEditableText* PlayerScore;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UEditableText* LastScoreHitTimer;
 };
