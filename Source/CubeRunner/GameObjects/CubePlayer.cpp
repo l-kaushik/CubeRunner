987942486379dff90b8d3474e0b90ba356c920f1
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-04-24 18:36:21 +0530

    create player class

---
+++ Source/CubeRunner/GameObjects/CubePlayer.cpp
@@ -0,0 +1,27 @@
+#include "CubePlayer.h"
+
+// Sets default values
+ACubePlayer::ACubePlayer()
+{
+	// Set this character to call Tick() every frame. You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACubePlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ACubePlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ACubePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/CubeRunner/GameObjects/CubePlayer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CubePlayer.generated.h"
+
+UCLASS()
+class CUBERUNNER_API ACubePlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACubePlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
