35dca65d9b0b3fe040425bfb56ec3dacef8cac01
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-04-26 18:55:22 +0530

    remove unused headers

--- Source/CubeRunner/UIs/PlayerHud.cpp
+++ Source/CubeRunner/UIs/PlayerHud.cpp
@@ -1,8 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "kismet/KismetStringLibrary.h"
-#include "kismet/KismetTextLibrary.h"
 #include "Components/EditableText.h"
 #include "PlayerHud.h"
 
