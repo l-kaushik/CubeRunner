149214dba6e139ebb214ba78f4af73b6783434e2
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-04-29 18:30:15 +0530

    add functions and variables

--- Source/CubeRunner/UIs/Assets/CustomButton.cpp
+++ Source/CubeRunner/UIs/Assets/CustomButton.cpp
@@ -1,1 +1,46 @@
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "Fonts/SlateFontInfo.h"
+#include "Styling/SlateColor.h"
+#include "Math/Color.h"
 #include "CustomButton.h"
+
+UCustomButton::UCustomButton(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	TextColor = FLinearColor::Black;
+	BackgroundColor = FLinearColor::White;
+}
+
+void UCustomButton::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	if (CustomButton)
+	{
+		CustomButton->SetColorAndOpacity(TextColor);
+		CustomButton->SetBackgroundColor(BackgroundColor);
+	}
+
+	if (ButtonText)
+	{
+		ButtonText->SetText(Text);
+		ButtonText->SetFont(Font);
+		ButtonText->SetRenderTransform(Transform);
+	}
+
+	if (CustomButton)
+	{
+		// Setting up Event
+		CustomButton->OnClicked.AddDynamic(this, &UCustomButton::InternalOnButtonClicked);
+	}
+}
+
+void UCustomButton::ClickButton()
+{
+	InternalOnButtonClicked();
+}
+
+void UCustomButton::InternalOnButtonClicked()
+{
+	OnButtonClicked.Broadcast();
+}
--- Source/CubeRunner/UIs/Assets/CustomButton.h
+++ Source/CubeRunner/UIs/Assets/CustomButton.h
@@ -4,9 +4,56 @@
 #include "Blueprint/UserWidget.h"
 #include "CustomButton.generated.h"
 
+class FText;
+struct FSlateColor;
+struct FSlateFontInfo;
+struct FWidgetTransform;
+struct FLinearColor;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnButtonClickedSignature);
+
 UCLASS()
 class CUBERUNNER_API UCustomButton : public UUserWidget
 {
 	GENERATED_BODY()
+	UCustomButton(const FObjectInitializer& ObjectInitializer);
 
+	virtual void NativePreConstruct();
+
+public:
+
+	UPROPERTY(BlueprintAssignable, Category = "Button")
+	FOnButtonClickedSignature OnButtonClicked;
+
+	UFUNCTION(BlueprintCallable, Category = "Button")
+	void ClickButton();
+
+protected:
+	
+	UPROPERTY(meta = (BindWidget))
+	class UButton* CustomButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* ButtonText;
+
+	// CustomButton Attribute
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ButtonText")
+	FLinearColor TextColor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ButtonText")
+	FLinearColor BackgroundColor;
+
+	// ButtonText Attribute
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ButtonText")
+	FText Text;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ButtonText")
+	FSlateFontInfo Font;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ButtonText")
+	FWidgetTransform Transform;
+
+	UFUNCTION()
+	void InternalOnButtonClicked();
 };
