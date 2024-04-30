54f76f9321fdee9eb0a6a28fc7c9f820817e28b5
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-04-30 18:32:45 +0530

    create UI related variables

--- Source/CubeRunner/UIs/Menus/OptionMenu.h
+++ Source/CubeRunner/UIs/Menus/OptionMenu.h
@@ -9,4 +9,63 @@ class CUBERUNNER_API UOptionMenu : public UUserWidget
 {
 	GENERATED_BODY()
 
+protected:
+	// remove BlueprintReadWrite later
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	class UComboBoxString* PresetSetting;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* IncrAliasing;	
+	
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* DecrAliasing;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* IncrFPS;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* DecrFPS;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* IncrResolution;	
+	
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* DecrResolution;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* IncrShader;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* DecrShader;
+	
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* IncrShadow;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* DecrShadow;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* IncrTexture;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* DecrTexture;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* IncrWindow;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* DecrWindow;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* IncrVSync;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* DecrVSync;	
+	
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* Back;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* ApplySettings;
+
 };
---
+++ enc_temp_folder/393a58c09cb3823cc2ef1276c6bf9d3/OptionMenu.cpp
@@ -0,0 +1,3 @@
+#include "Components/ComboBox.h"
+#include "Components/Button.h"
+#include "OptionMenu.h"
