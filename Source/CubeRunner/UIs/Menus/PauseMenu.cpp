0b41bcf58484b271099e84fb8c29bc8c9af6d397
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-05-21 20:17:44 +0530

    Implemented PauseMenu
    1. Implemented all 3 button Resume, Optoins and quit
    2. Implemented function binding also.

--- Source/CubeRunner/UIs/Menus/PauseMenu.cpp
+++ Source/CubeRunner/UIs/Menus/PauseMenu.cpp
@@ -1,1 +1,60 @@
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/Widget.h"
+
+#include "../Assets/CustomButton.h"
+#include "OptionMenu.h"
 #include "PauseMenu.h"
+
+void UPauseMenu::NativeConstruct()
+{
+	if (Resume)
+	{
+		Resume->OnButtonClicked.AddDynamic(this, &UPauseMenu::OnResumeClicked);
+	}
+
+	if (Options)
+	{
+		Options->OnButtonClicked.AddDynamic(this, &UPauseMenu::OnOptionsClicked);
+	}
+
+	if (QuitGameButton)
+	{
+		QuitGameButton->OnButtonClicked.AddDynamic(this, &UPauseMenu::OnQuitGameButtonClicked);
+	}
+}
+
+void UPauseMenu::OnResumeClicked()
+{
+	UGameplayStatics::SetGamePaused(GetWorld(), false);
+	
+	auto PlayerController = UGameplayStatics::GetPlayerController(UUserWidget::GetWorld(), 0);
+
+	if (PlayerController)
+	{
+		PlayerController->SetShowMouseCursor(true);
+		PlayerController->SetInputMode(FInputModeGameOnly());
+	}
+
+	UWidget::RemoveFromParent();
+}
+
+void UPauseMenu::OnOptionsClicked()
+{
+	// Remove this widget
+	UWidget::RemoveFromParent();
+
+	// Create option widget
+	APlayerController* FPC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	check(FPC);
+	auto OptionMenu = CreateWidget<UOptionMenu>(FPC, OptionMenuClassRef);
+	check(OptionMenu);
+	OptionMenu->PreviousWidgetReference = PauseMenuClassRef;
+	OptionMenu->AddToPlayerScreen();
+}
+
+void UPauseMenu::OnQuitGameButtonClicked()
+{
+	UGameplayStatics::GetPlayerController(GetWorld(), 0)->ConsoleCommand("quit");
+}
--- Source/CubeRunner/UIs/Menus/PauseMenu.h
+++ Source/CubeRunner/UIs/Menus/PauseMenu.h
@@ -4,9 +4,38 @@
 #include "Blueprint/UserWidget.h"
 #include "PauseMenu.generated.h"
 
+class UOptionMenu;
+
 UCLASS()
 class CUBERUNNER_API UPauseMenu : public UUserWidget
 {
 	GENERATED_BODY()
 
+protected:
+	virtual void NativeConstruct();
+
+protected:
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UCustomButton* Resume;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UCustomButton* Options;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UCustomButton* QuitGameButton;
+
+	UPROPERTY(EditAnywhere, Category = "Class References")
+	TSubclassOf<class UOptionMenu> OptionMenuClassRef;	
+	
+	UPROPERTY(EditAnywhere, Category = "Class References")
+	TSubclassOf<class UPauseMenu> PauseMenuClassRef;
+
+	UFUNCTION()
+	void OnResumeClicked();
+
+	UFUNCTION()
+	void OnOptionsClicked();
+
+	UFUNCTION()
+	void OnQuitGameButtonClicked();
 };
