b114f90876c35b167a03c6dd78c535049c3475c7
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-05-28 18:00:08 +0530

    Removed extra indent

--- Source/CubeRunner/GameObjects/SafeCube.h
+++ Source/CubeRunner/GameObjects/SafeCube.h
@@ -20,6 +20,5 @@ protected:
 	virtual void BeginPlay() override;
 
 protected:
-		void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
-	
+	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;	
 };
